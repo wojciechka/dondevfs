59d150c9806712664a5bda10c9bc17c993ce58b4
Wojciech Kocjan <wojciech@kocjan.org>
2014-11-25 15:15:14 +0100

    Cleanup of code

--- lib/dondevfs/s3cmd.tcl
+++ lib/dondevfs/s3cmd.tcl
@@ -14,8 +14,9 @@ proc dondevfs::s3cmd::mount {path args} {
     array set o {
         volume  0
         debug   0
-	s3path  {}
-	s3cmd   {s3cmd}
+        envvars {}
+        s3path  {}
+        s3cmd   {s3cmd}
     }
     set validUsage 1
     while {[llength $args] > 0} {
@@ -28,32 +29,41 @@ proc dondevfs::s3cmd::mount {path args} {
                 set o(debug) 1
                 set args [lrange $args 1 end]
             }
-	    -ac - -auto - -autocommand {
-		# workaround for scripts that change PATH and may interfere
-		# with s3cmd invocation
-		set _s3cmd [lindex [auto_execok s3cmd] end]
-		if {[file exists $_s3cmd]} {
-		    set fh [open $_s3cmd r]
-		    gets $fh shebang
-		    close $fh
-		    set s3python [auto_execok python]
-		    if {[regexp {^#!(.*)$} $shebang - s3python]} {
-			set s3python [string trim $s3python]
-			regsub {/usr/bin/env\s+} $s3python {} s3python
-			set s3python [auto_execok [lindex $s3python 0]]
-		    }
-		    set o(s3cmd) [concat $s3python $_s3cmd]
-		}
+            -ac - -auto - -autocommand {
+                # workaround for scripts that change PATH and may interfere
+                # with s3cmd invocation
+                set _s3cmd [lindex [auto_execok s3cmd] end]
+                if {[file exists $_s3cmd]} {
+                    set fh [open $_s3cmd r]
+                    gets $fh shebang
+                    close $fh
+                    set s3python [auto_execok python]
+                    if {[regexp {^#!(.*)$} $shebang - s3python]} {
+                        set s3python [string trim $s3python]
+                        regsub {/usr/bin/env\s+} $s3python {} s3python
+                        set s3python [auto_execok [lindex $s3python 0]]
+                    }
+                    set o(s3cmd) [concat $s3python $_s3cmd]
+                }
                 set args [lrange $args 1 end]
-	    }
-	    -s3 - -s3path - -remote {
+            }
+            -s3 - -s3path - -remote {
                 set o(s3path) [lindex $args 1]
                 set args [lrange $args 2 end]
-	    }
-	    -cfg - -s3cfg {
+            }
+            -cfg - -s3cfg {
                 set o(s3cfg) [lindex $args 1]
                 set args [lrange $args 2 end]
-	    }
+            }
+            -p - -preserve {
+                # preserve environment to avoid changes to system settings breaking s3cmd
+                foreach ev {PATH LD_LIBRARY_PATH DYLD_LIBRARY_PATH} {
+                    if {[catch {lappend o(envvars) $ev 1 $::env($ev)}]} {
+                        lappend o(envvars) $ev 0 {}
+                    }
+                }
+                set args [lrange $args 1 end]
+            }
             default {
                 set validUsage 0
                 break
@@ -74,28 +84,29 @@ proc dondevfs::s3cmd::mount {path args} {
     lappend cmd -opencommand dondevfs::s3cmd::opencmd
     lappend cmd -unmountcommand dondevfs::s3cmd::_unmount
     if {$o(volume)} {
-	lappend cmd -volume
+        lappend cmd -volume
     }
     if {$o(debug)} {
-	lappend cmd -debug
+        lappend cmd -debug
     }
     set var [uplevel #0 $cmd]
     upvar #0 $var v
     set v(s3cmd-s3path) [string trimright $o(s3path) /]
     set v(s3cmd-s3cfg) $o(s3cfg)
     set v(s3cmd-s3cmd) $o(s3cmd)
+    set v(s3cmd-envvars) $o(envvars)
     if {![file exists $v(s3cmd-s3cfg)]} {
-	set v(s3cmd-s3cfg) [file normalize ~/.s3cfg]
+        set v(s3cmd-s3cfg) [file normalize ~/.s3cfg]
     }
     if {![file exists $v(s3cmd-s3cfg)]} {
-	catch {vfs::unmount $path}
-	error "Unable to find .s3cfg file"
+        catch {vfs::unmount $path}
+        error "Unable to find .s3cfg file"
     }
     if {[catch {
-	_init $var
+        _init $var
     } err]} {
-	catch {vfs::unmount $path}
-	error "Unable to query s3cmd file listing: $err"
+        catch {vfs::unmount $path}
+        error "Unable to query s3cmd file listing: $err"
     }
 }
 
@@ -104,10 +115,9 @@ proc dondevfs::s3cmd::_unmount {var} {
     unset ${var}.f
 }
 
-proc dondevfs::s3cmd::_init {var} {
-    upvar #0 $var v ${var}.g g ${var}.f f
-    set output [_s3cmd $var 600 ls -r "$v(s3cmd-s3path)/"]
-    set idx [expr {[string length $v(s3cmd-s3path)] + 1}]
+proc dondevfs::s3cmd::parseLsOutput {output prefix} {
+    upvar f f g g
+    set idx [expr {[string length $prefix] + 1}]
     foreach line [split [string trim $output] \n] {
         if {[regexp {^(.*?)(s3://.*)$} [string trim $line] - info p]} {
             set p [string range $p $idx end]
@@ -145,41 +155,68 @@ proc dondevfs::s3cmd::_init {var} {
     }
 }
 
+proc dondevfs::s3cmd::_init {var} {
+    upvar #0 $var v ${var}.g g ${var}.f f
+    set output [_s3cmd $var 600 ls -r "$v(s3cmd-s3path)/"]
+    parseLsOutput $output $v(s3cmd-s3path)
+}
+
+proc dondevfs::s3cmd::_envvars {var list} {
+    upvar #0 $var v
+    set rc {}
+    foreach {ev exists value} $list {
+        if {[catch {lappend rc $ev 1 $::env($ev)}]} {
+            lappend rc $ev 0 {}
+        }
+        if {$exists} {
+            set ::env($ev) $value
+        }  else  {
+            catch {unset ::env($ev)}
+        }
+    }
+    return $rc
+}
+
 proc dondevfs::s3cmd::_s3cmd {var timeout args} {
     upvar #0 $var v
     set rc {}
     set ok 0
+    set revertVars [_envvars $var $v(s3cmd-envvars)]
     foreach try {1 2 3} {
-	set rc {}
-	set ctimeout [expr {[clock seconds] + $timeout}]
-	if {[catch {
-	    set fh [open "|[concat $v(s3cmd-s3cmd) [list -c $v(s3cmd-s3cfg)] $args [list </dev/null 2>@1]]" r]
-	    fconfigure $fh -blocking 0 -buffering none
-	} err]} {
-	    continue
-	}
-	set ok 0
-	# the read has to be done as sync operation not to mess with Tcl event loop
-	while {[clock seconds] < $ctimeout} {
-	    after 500
-	    catch {set eof 1 ; append rc [read $fh] ; set eof [eof $fh]}
-	    if {$eof} {
-		fconfigure $fh -blocking 1 -buffering none
-		if {![catch {close $fh} err]} {
-		    set ok 1
-		}
-		break
-	    }
-	}
-	if {$ok} {
-	    break
-	} else {
-	    after ${try}0000
-	    continue
-	}
+        set rc {}
+        set ctimeout [expr {[clock seconds] + $timeout}]
+        if {[catch {
+            set fh [open "|[concat $v(s3cmd-s3cmd) [list -c $v(s3cmd-s3cfg)] $args [list </dev/null 2>@1]]" r]
+            fconfigure $fh -blocking 0 -buffering none
+        } err]} {
+            continue
+        }
+        set ok 0
+        # the read has to be done as sync operation not to mess with Tcl event loop
+        while {[clock seconds] < $ctimeout} {
+            after 500
+            catch {set eof 1 ; append rc [read $fh] ; set eof [eof $fh]}
+            if {$eof} {
+                fconfigure $fh -blocking 1 -buffering none
+                if {![catch {close $fh} err]} {
+                    set ok 1
+                }
+                break
+            }
+        }
+        if {$ok} {
+            break
+        } else {
+            after ${try}0000
+            continue
+        }
     }
+    _envvars $var $revertVars
     if {!$ok} {
-	error "Unable to run s3cmd: $err"
+        if {$v(debug)} {
+            puts "Error during download: $err\n$rc"
+        }
+        error "Unable to run s3cmd: $err"
     }
     return $rc
 }
@@ -208,31 +245,53 @@ proc dondevfs::s3cmd::opencmd {var path} {
     set download 1
     set vpath [file join $v(path) $path]
     if {[file exists $vpath]} {
-	set download 0
-	file stat $vpath ls
-	foreach t {type size mtime} {
-	    if {$ls($t) != $s($t)} {
-		set download 1
-	    }
-	}
+        set download 0
+        file stat $vpath ls
+        foreach t {type size mtime} {
+            if {$ls($t) != $s($t)} {
+                set download 1
+            }
+        }
     }
     if {$download} {
-        download $var $path $vpath
-	# set mtime to ensure it is valid next time
-	file mtime $vpath $s(mtime)
+        if {[catch {
+            download $var $path $vpath $s(mtime)
+        } err]} {
+            if {$v(debug)} {
+                puts "Error during download: $err"
+            }
+        }
     }
     return [open $vpath]
 }
 
 
-proc dondevfs::s3cmd::download {var path vpath} {
+proc dondevfs::s3cmd::download {var path vpath mtime} {
     upvar #0 $var v
     set ok 0
+
     array set s [statcmd $var $path]
     # assume at least 100kB/s transfer and 60s gap
     set timeout [expr {60 + int($s(size) / 100000)}]
     file mkdir [file dirname $vpath]
-    _s3cmd $var $timeout get -f $v(s3cmd-s3path)/$path $vpath
+    set tmppath $vpath.[pid]
+    catch {file delete -force $tmppath}
+    _s3cmd $var $timeout get -f $v(s3cmd-s3path)/$path $tmppath
+    if {[catch {file rename $tmppath $vpath} err]} {
+        set ei $::errorInfo
+        set ec $::errorCode
+        # another process also downloaded and renamed the file - simply
+        # cleanup temp file and assume it will set mtime properly
+        if {([lindex $ec 0] == "POSIX") && ([lindex $ec 1] == "EEXIST")} {
+            catch {file delete -force $tmppath}
+        }  else  {
+            catch {file delete -force $tmppath}
+            error $err $ei $ec
+        }
+    }  else  {
+        # set mtime to ensure it is valid next time
+        file mtime $vpath $mtime
+    }
 }
 
 package provide dondevfs::s3cmd 1.0
